1d2a18d065a3500b9e6c3da8c93c928ffa67c69e
adarshaharshavardhana@gmail.com <adarshaharshavardhana@gmail.com>
2025-07-15 00:54:54 +0530

    ready for deployment4

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ const App = () => {
         <FiArrowDown size={20} className="animate-bounce" />
       </button>
 <Marquee/>
-
+<div className="h-7"></div>
       {/* Hero Section */}
       <Animatedslider scrollToContact={scrollToContact} bgChanged={bgChanged} />
 
